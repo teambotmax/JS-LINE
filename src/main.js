0ec8dcf17f4a74b8c8c87d644f49e0ccbdee6393
AsockA <50161530+Asocka@users.noreply.github.com>
2019-09-13 11:01:40 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -15,7 +15,7 @@ class LINE extends Command {
     }
 
     get myBot() {
-        const bot = ['ufe1707ae9b2ff7ab61505795b7995440','u6949f816dddef051b5762322e5597f1e'];
+        const bot = ['u83bf1879ab42e6db1a85b755bf6d8d0e','u83bf1879ab42e6db1a85b755bf6d8d0e'];
         return bot; 
     }
 
